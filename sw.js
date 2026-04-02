d9acec11e872e0059d43760b07acd0325dcec4b7
AwReel-Ink <awreeldroid@gmail.com>
2026-04-02 22:40:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'vi-turette-v1.0.1';
 const ASSETS_TO_CACHE = [
   './',
-  './VI-Turette.html',
+  './index.html',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png'
